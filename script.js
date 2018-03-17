db8002af83f15814927dc0f74f8f1f59a0b65354
almowafy <abdulrahmanalmowafy44@gmail.com>
2018-03-17 08:56:10 +0200

    module 4

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 (function () {
   var app = angular.module("githubViewer", []);
 
-  var controllerCallBack = function ($scope, $http) {
-    $scope.sortOrder = "-stargazers_count"
+  var controllerCallBack = function ($scope, $interval, $log, $location, $anchorScroll, github) {
+    var countdownInterval = null;
     $scope.username = "angular";
     $scope.title = "GitHub Viewer";
+    $scope.sortOrder = "-stargazers_count"
+    $scope.countdown = 5;
 
-    var onReposComplete = function(response){
+    var onReposComplete = function(data){
       $scope.error = null;
-      $scope.repos = response.data;
+      $scope.repos = data;
+      $location.hash("userDetails");
+      $anchorScroll();
     };
 
-    var onUserComplete = function (response) {
+    var onUserComplete = function (data) {
       $scope.error = null;
-      $scope.user = response.data;
-      $http.get(response.data.repos_url)
+      $scope.user = data;
+      github.getRepos(data)
         .then(onReposComplete, onError);
     };
 
@@ -22,11 +26,27 @@
       $scope.error = "Could not fetch data";
     };
 
-    $scope.search = function (username) {
-      $http.get("https://api.github.com/users/" + username)
+    var reduceCountDown = function(){
+      $scope.countdown--;
+      if($scope.countdown==0){
+        $scope.search();
+      }
+    }
+
+    var startCountdown = function(){
+      countdownInterval = $interval(reduceCountDown, 1000, $scope.countdown);
+    }
+
+    $scope.search = function () {
+      $log.info("searching for " + $scope.username);
+      $interval.cancel(countdownInterval);
+      $scope.countdown = null;
+      github.getUser($scope.username)
         .then(onUserComplete, onError);
     }
+
+    startCountdown();
   };
 
-  app.controller("MainController", ["$scope", "$http", controllerCallBack]);
+  app.controller("MainController", controllerCallBack);
 }());
